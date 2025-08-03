77b3cd2703f6113f59e0d4638e7dc2c91096c3a7
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-08-03 17:54:52 -0400

    added the basic movement

--- player.gd
+++ player.gd
@@ -1,1 +1,23 @@
 extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity") * delta
+
+	# Handle jump
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Handle horizontal movement
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Move the character
+	move_and_slide()
