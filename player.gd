0325a9e1ac133c25ee488c9899d5e94dea814eba
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-08-03 18:33:26 -0400

    WOOO walk and idle animation

--- player.gd
+++ player.gd
@@ -2,22 +2,28 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const GRAVITY = 980.0
+
+@onready var animated_sprite = $Player
 
 func _physics_process(delta: float) -> void:
-	# Apply gravity
+	# Add gravity
 	if not is_on_floor():
-		velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity") * delta
+		velocity.y += GRAVITY * delta
 
 	# Handle jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Handle horizontal movement
+	# Horizontal movement
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animated_sprite.play("Walk")
+		animated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite.play("Idle")
 
 	# Move the character
 	move_and_slide()
